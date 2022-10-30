6208ee7ce0b8d059d6f60b57fb541be746ebe0a5
Evgenia <jenya.fenina@yandex.ru>
2022-10-30 23:29:07 +0300

    Fix comments

--- project/src/pages/not-found-page/not-found-page.tsx
+++ project/src/pages/not-found-page/not-found-page.tsx
@@ -1,45 +1,18 @@
+import styles from './not-found-page.module.css';
+import Header from '../../components/header/header';
+import Footer from '../../components/footer/footer';
+
 function NotFoundPage(): JSX.Element {
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="/#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="/#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header />
 
-      <section className="container" style={{textAlign: 'center'}}>
-        <h1>404. Page not found</h1>
+      <section className={styles.section}>
+        <h1 className={styles.h1}>404. Page not found</h1>
         <a href="/">Вернуться на главную</a>
       </section>
 
-      <footer className="footer container">
-        <a className="footer__logo-link" href="main.html">
-          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
